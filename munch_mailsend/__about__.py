d4e7098396e2badd50b5acbad1b3b1d786d2093b
Geoffrey Lehée <glehee@oasiswork.fr>
2016-12-01 09:51:24 +0100

    Update setup.cfg and add new about file

---
+++ munch_mailsend/__about__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "munch-mailsend"
+__summary__ = "Sending mass emails with Munch."
+__uri__ = "https://github.com/crunchmail/munch-mailsend"
+
+__version__ = "0.1.0"
+
+__author__ = u"Crunchmail"
+__email__ = "dev@crunchmail.com"
+
+__license__ = "GNU AGPLv3"
+__copyright__ = "Copyright 2016 %s" % __author__
